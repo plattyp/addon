898b08fdd516e94837c49d8716ae7cf12c60a74d
Andrew Platkin <andrew.platkin@gmail.com>
2017-03-19 20:45:52 -0500

    fix spacing

--- db/migrations/20170319150145-create_users.sql
+++ db/migrations/20170319150145-create_users.sql
@@ -5,7 +5,7 @@ CREATE TABLE public.users (
 	"email" varchar(255) NOT NULL COLLATE "default",
 	"encrypted_password" text COLLATE "default",
 	"password_salt" text COLLATE "default",
-  "created_at" timestamp(6) WITH TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
+	"created_at" timestamp(6) WITH TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
 	"updated_at" timestamp(6) WITH TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
 	"deleted_at" timestamp(6) WITH TIME ZONE
 );
